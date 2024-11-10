a9d32abcc7d31c3601a2bc4853683c90c32176fb
Natouche68 <ngrossheny@gmail.com>
2024-11-10 18:15:18 +0100

    Create a file server

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World !")
-	})
+	fs := http.FileServer(http.Dir("public"))
+
+	http.Handle("/", fs)
 
 	http.ListenAndServe(":5173", nil)
 }
