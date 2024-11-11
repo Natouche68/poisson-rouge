2e8b1fd11e5bef3f305222080491cbd9519cb68b
Natouche68 <ngrossheny@gmail.com>
2024-11-11 16:11:18 +0100

    Add some test data

--- api/api.go
+++ api/api.go
@@ -8,5 +8,5 @@ import (
 var getNotesTmpl = template.Must(template.ParseFiles("templates/getNotes.html"))
 
 func GetNotes(w http.ResponseWriter, r *http.Request) {
-	getNotesTmpl.Execute(w, nil)
+	getNotesTmpl.Execute(w, struct{ Notes []string }{Notes: []string{"Note 1", "Note 2", "Note 3", "Note 4", "Note 5", "Note 6"}})
 }
